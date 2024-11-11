44c73be09dfe360d4e39a03a030666c8ec814238
Clarence Kimanga <kimangaclarenc@gmail.com>
2024-11-11 09:41:10 +0300

    Answer to assigment one

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # assignment-1-python-programming
+This is a simple Python program that asks a user for their name, age, and location and then prints out a personalized message. 
---
+++ user-input.py
@@ -0,0 +1,11 @@
+# Prompting the user for their name
+user_name = input("Enter your name: ")
+
+# Prompting the user for their age
+user_age = input("Enter your age: ")
+
+# Prompting the user for their location
+user_location = input("Enter your location: ")
+
+# Displaying a welcome message with the collected information
+print(f"Welcome {user_name}. You are {user_age} years old, and you live in {user_location}.")
